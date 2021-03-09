5567bb7cefecc64cb57e66874847ba37056a0e5f
Nico Rithner <nicorithner@gmail.com>
2021-03-09 12:53:37 -0700

    Add some tests

--- test/controllers/transfers_controller_test.rb
+++ test/controllers/transfers_controller_test.rb
@@ -1,23 +1,31 @@
 require 'test_helper'
 
 class TransfersControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get transfers_index_url
-    assert_response :success
+  setup do
+    @school = School.create(name: "Test school", location: "Test address", description: "Test description")
+    @curriculum = Curriculum.create(title: "Curriculum test", description: "Curriculum description", capacity: 5)
+    @school.teachers << @teacher = teachers(:one)
+    @school.cohorts << @cohort = Cohort.create(name: "Cohort test", school_id: @school.id, teacher_id: @teacher.id, curriculum_id: @curriculum.id)
+    @school.students << @student = students(:one)
+    #binding.pry
+    @transfer = Transfer.create!(cohort_id: @cohort.id,student_id: @student.id, school_id: @school.id, start_date: Date.today, end_date: Date.today + 90)
   end
-
-  test "should get show" do
-    get transfers_show_url
-    assert_response :success
-  end
-
+  
   test "should get new" do
-    get transfers_new_url
+    get new_transfer_path(@transfer.id)
     assert_response :success
   end
 
   test "should get create" do
-    get transfers_create_url
+    assert_difference('Transfer.count') do
+      post transfers_path, params: { transfer: { cohort_id: @cohort.id,student_id: @student.id, school_id: @school.id, start_date: Date.today, end_date: Date.today + 90}}
+      end
+
+    assert_redirected_to school_enrollment_path(@school, Enrollment.last)
+  end
+
+  test "should get show" do
+    get transfer_path(@transfer)
     assert_response :success
   end
 
